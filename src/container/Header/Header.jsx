69c29792945cd781d4561bd38f7e9367cce9c0e0
ravi gupta <ravi2611gupta@gmail.com>
2022-10-09 19:59:40 +0530

    created whole folder structure and navbar

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {SubHeading} from '../../components'
+
+import './Header.css';
+
+const Header = () => (
+  <div className='app__header app__wrapper section__padding' id="home">
+    <div className='app__wrapper_info'>
+      <SubHeading/>
+    </div>
+    <div className="app__wrapper_img">
+
+    </div>
+  </div>
+);
+
+export default Header;
