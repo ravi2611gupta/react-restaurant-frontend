1c7f61448d8db84de7ce31ff9685f9161a7cd84a
ravi gupta <ravi2611gupta@gmail.com>
2022-10-09 22:53:15 +0530

    chef section done

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-
+import {images} from "../../constants"
+import {SubHeading} from "../../components"
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word" />
+      <h1 className='headtext__cormorant'>What we believe in</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className='p__opensans'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi accusantium nemo architecto enim.
+          </p>
+        </div>
+        <p className='p__opensans'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit facere ratione, facilis exercitationem dolores voluptate qui odit eius sit cum impedit atque beatae ipsam corrupti dolore tempora reiciendis fuga ipsa!
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+
+    </div>
   </div>
 );
 
